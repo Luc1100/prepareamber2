c9ea409fc50a0514677be017d38bd4006780bee4
Luc1100 <52473174+Luc1100@users.noreply.github.com>
2021-10-31 22:21:59 -0400

    structure input

--- prepareamber2.py
+++ prepareamber2.py
@@ -1,11 +1,36 @@
-from pdbfixer import *
+import argparse
+from pdbfixer import PDBFixer
+import pdb_util as util
+import os
 
+
+def fix_pdb(structures):
+    for strucutre in structures:
+        if 'pdb' in os.path.splitext(structure)[-1]:
+            #the "structure" string in the args.structure list will be updated so
+            #that it corresponds to the PDB we should use for subsequent steps
+            assert os.path.isfile(pdb),'%s does not exist\n' % structure
+
+            #use pdbfixer to find anything wrong with initial pdb(s)
+            fixer = PDBFixer(filename=structure)
+            fixer.findMissingResidues()
+            print(fixer.missingResidues)
+            fixer.addMissingAtoms()
+        
 
 def main():
-    fixer = PDBFixer(filename='complex_noH.pdb')
-    fixer.findNonstandardResidues()
-    print(fixer.nonstandardResidues)
+    parser = argparse.ArgumentParser(description="Generates pre-production files for AMBER \
+    MD. Can handle a receptor or ligand by themselves (checks for ligand library \
+    files in the current directory and generates them if they don't exist) or \
+    sets up the complex if given both a receptor and ligand.")
 
+    parser.add_argument('-s', '--structures', nargs='+', required=True, help='Structures \
+    for which you want to run a simulation. N.B. if more than one is provided \
+    they will be simulated together.')
+
+    args = parser.parse_args()
+
+    fix_pdb(args.structures)
 
 if __name__ == '__main__':
     main()
