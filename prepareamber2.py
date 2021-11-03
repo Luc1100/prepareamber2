41714ffe5b63bef4755090225274862de0c66a98
Luc1100 <52473174+Luc1100@users.noreply.github.com>
2021-11-02 21:48:00 -0400

    started working on finding nonstandard libraries

--- prepareamber2.py
+++ prepareamber2.py
@@ -1,6 +1,14 @@
 import argparse
 from pdbfixer import PDBFixer
-import os
+import os, glob
+
+
+'''
+Strip path and extension from filename in order to generate other filenames
+'''
+def get_base(fname):
+    return os.path.splitext(os.path.basename(fname))[0]
+
 
 
 def fix_pdb(structures):
@@ -18,11 +26,21 @@ def fix_pdb(structures):
             # add missing residues and atoms
             print('Adding missing residues and atoms...')
             fixer.addMissingAtoms()
+
             # TODO: deal with nonstandard residues
             fixer.findNonstandardResidues()
-            print(fixer.nonstandardResidues)
 
-        
+            # if modifications files not provided
+            # if not args.libs:
+            #     args.libs = []
+            # find_nonstandard_libraries(structure, args.libs, fixer.nonstandardResidues)
+            # for res in fixer.nonstandardResidues:
+            #     print('Unknown residue {name} at {id} in {chain}. '\
+            #         'Suggested replacement: {replacement}'.format(
+            #         name=res[0].name, id=res[0].id, chain=res[0].chain, replacement=res[1]))
+
+            
+
 
 def main():
     parser = argparse.ArgumentParser(description="Generates pre-production files for AMBER \
@@ -33,6 +51,17 @@ def main():
     parser.add_argument('-s', '--structures', nargs='+', required=True, help='Structures \
     for which you want to run a simulation. N.B. if more than one is provided \
     they will be simulated together.')
+
+    parser.add_argument('-p', '--libs', nargs='+', required=False, help="Optionally specify \
+    a prefix for nonstandard residue library files; this can include their path if they aren't \
+    in the current directory. If the relevant files exist we assume you want \
+    to use them, otherwise we assume this is where you want them to go and \
+    derive the residue name accordingly.")
+
+    parset.add_argument('-ns', '--replace_nonstandard', default=False, action='store_true',
+    help="Replace nonstandard residues with suggested replacement if nonstandard residue libraries \
+    are not found and not provided. Deafult is false, becuase you may not have known there are \
+    missing libraries.")
 
     args = parser.parse_args()
 
